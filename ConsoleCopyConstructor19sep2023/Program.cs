ad8efe45c285593a9dd7f1956d867e62e5f51c82
kris_borre <kris_borre@DESKTOP-V0LQE95>
2023-09-19 16:02:03 +0200

    Add project files.

---
+++ ConsoleCopyConstructor19sep2023/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Student
+{
+    private string name;
+    private int rank;
+
+    public Student(Student s)
+    {
+        name = s.name;
+        rank = s.rank;
+    }
+
+    public Student(string name, int rank)
+    {
+        this.name = name;
+        this.rank = rank;
+    }
+
+    public string Display
+    {
+        get
+        {
+            return " Student " + name + " got Rank " + rank.ToString();
+        }
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Copy Constructor!");
+
+        Student s1 = new Student("Jack", 2);
+
+        // copy constructor
+        Student s2 = new Student(s1);
+
+        // display
+        Console.WriteLine(s2.Display);
+        Console.ReadLine();
+    }
+}
