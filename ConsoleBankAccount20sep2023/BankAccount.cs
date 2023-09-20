4ae321fdc13194471a6974ab8ad95f3fff1a3b8b
kris_borre <kris_borre@DESKTOP-V0LQE95>
2023-09-20 11:49:00 +0200

    enum

---
+++ ConsoleBankAccount20sep2023/BankAccount.cs
@@ -0,0 +1,94 @@
+﻿namespace ConsoleBankAccount20sep2023
+{
+    internal class BankAccount
+    {
+        public enum AccountState
+        {
+            Geldig, Geblokkeerd
+        }
+
+        private AccountState accountState = AccountState.Geldig;
+
+        private string naam;
+
+        public string Naam
+        {
+            get { return naam; }
+            set { naam = value; }
+        }
+
+        private int bedrag;
+
+        protected int Bedrag
+        {
+            get { return bedrag; }
+            set { bedrag = value; }
+        }
+
+        private string rekeningnummer;
+
+        public string Rekeningnummer
+        {
+            get { return rekeningnummer; }
+            set { rekeningnummer = value; }
+        }
+
+        /// <summary>
+        /// WithdrawFunds: bepaald bedrag wordt van rekening verwijderd.
+        /// Pas de WithdrawFunds methode aan zodat als returntype het bedrag (int) wordt teruggegeven.
+        /// Indien het gevraagde bedrag meer dan de balance is dan geef je al het geld terug dat nog op de rekening staat en toon je in de console dat niet al het geld kon worden gegeven.
+        /// </summary>
+        public int WithdrawFunds(int bepaaldBedrag)
+        {
+            int withdrawnAmount = 0;
+            if (bepaaldBedrag > Bedrag)
+            {
+                Console.WriteLine("Excuses, Er staat onvoldoende op de rekening.");
+                Console.WriteLine($"Er staat slechts {GetBalance()} op de rekening.");
+                Console.WriteLine("Niet al het geld kon worden gegeven.");
+                withdrawnAmount = Bedrag;
+                Bedrag = 0;
+            }
+            else
+            {
+                Bedrag -= bepaaldBedrag;
+                withdrawnAmount = bepaaldBedrag;
+            }
+            return withdrawnAmount;
+        }
+
+
+        /// <summary>
+        /// PayInFunds: bepaald bedrag (als parameter) wordt op de rekening gezet.
+        /// </summary>
+        public void PayInFunds(int bepaaldBedrag)
+        {
+            Bedrag += bepaaldBedrag;
+        }
+
+        /// <summary>
+        /// GetBalance: het totale bedrag op de rekening wordt teruggegeven.
+        /// </summary>
+        public int GetBalance()
+        {
+            return Bedrag;
+        }
+
+        public void ChangeState(AccountState state)
+        {
+            accountState = state;
+        }
+
+        public static void Transactie(int bedrag, BankAccount from, BankAccount to)
+        {
+            if (from.accountState == AccountState.Geldig && to.accountState == AccountState.Geldig)
+            {
+                to.PayInFunds(from.WithdrawFunds(bedrag));
+            }
+            else
+            {
+                Console.WriteLine("Error: Een rekening is geblokkeerd.");
+            }
+        }
+    }
+}
---
+++ ConsoleBankAccount20sep2023/Program.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleBankAccount20sep2023;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welkom bij de BankAccount console application!");
+
+        BankAccount rekening1 = new BankAccount();
+        rekening1.Naam = "Van Looy";
+        rekening1.Rekeningnummer = "545488787";
+        rekening1.PayInFunds(1000);
+        Console.WriteLine($"Op de rekening van {rekening1.Naam} staat {rekening1.GetBalance()} euro.");
+
+        BankAccount rekening2 = new BankAccount();
+        rekening2.Naam = "Bernard";
+        rekening2.Rekeningnummer = "332232314112";
+        rekening2.PayInFunds(1500);
+        Console.WriteLine($"Op de rekening van {rekening2.Naam} staat {rekening2.GetBalance()} euro.");
+
+        Console.WriteLine($"Storting rekening {rekening2.Naam} naar rekening {rekening1.Naam}.");
+        rekening1.PayInFunds(rekening2.WithdrawFunds(200));
+
+        Console.WriteLine($"Op de rekening van {rekening1.Naam} staat {rekening1.GetBalance()} euro.");
+        Console.WriteLine($"Op de rekening van {rekening2.Naam} staat {rekening2.GetBalance()} euro.");
+
+        Console.WriteLine($"Poging van storting rekening {rekening2.Naam} naar rekening {rekening1.Naam}.");
+        rekening1.PayInFunds(rekening2.WithdrawFunds(2000));
+
+        Console.WriteLine($"Op de rekening van {rekening1.Naam} staat {rekening1.GetBalance()} euro.");
+        Console.WriteLine($"Op de rekening van {rekening2.Naam} staat {rekening2.GetBalance()} euro.");
+
+        Console.WriteLine($"Storting rekening {rekening1.Naam} naar rekening {rekening2.Naam}.");
+        BankAccount.Transactie(100, rekening1, rekening2);
+
+        Console.WriteLine($"Op de rekening van {rekening1.Naam} staat {rekening1.GetBalance()} euro.");
+        Console.WriteLine($"Op de rekening van {rekening2.Naam} staat {rekening2.GetBalance()} euro.");
+
+        rekening1.ChangeState(BankAccount.AccountState.Geblokkeerd);
+        Console.WriteLine($"Poging tot storting rekening {rekening1.Naam} naar rekening {rekening2.Naam}.");
+        BankAccount.Transactie(500, rekening1, rekening2);
+        rekening1.ChangeState(BankAccount.AccountState.Geldig);
+
+        Console.WriteLine($"Op de rekening van {rekening1.Naam} staat {rekening1.GetBalance()} euro.");
+        Console.WriteLine($"Op de rekening van {rekening2.Naam} staat {rekening2.GetBalance()} euro.");
+
+        Console.ReadLine();
+    }
+}
