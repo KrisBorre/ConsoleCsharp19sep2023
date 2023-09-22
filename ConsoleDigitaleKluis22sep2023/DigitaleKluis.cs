b30cfa3539ea6792fe1dce84e88795193ceed6df
kris_borre <kris_borre@DESKTOP-V0LQE95>
2023-09-22 09:08:17 +0200

    private set

---
+++ ConsoleDigitaleKluis22sep2023/DigitaleKluis.cs
@@ -0,0 +1,99 @@
+﻿namespace ConsoleDigitaleKluis22sep2023
+{
+    internal class DigitaleKluis
+    {
+        /// <summary>
+        /// Een private variabele die de toegangscode van de kluis bewaard als geheel getal
+        /// </summary>
+        private int code;
+
+        /// <summary>
+        /// Een overloaded constructor die als parameter een geheel getal toelaat. 
+        /// Dit getal zal worden toegewezen aan de private variabele code.
+        /// </summary>
+        /// <param name="getal"></param>
+        public DigitaleKluis(int getal)
+        {
+            CanShowCode = false;
+            Code = getal;
+            CodeLevel = getal / 1000;
+        }
+
+        private bool canShowCode;
+
+        /// <summary>
+        /// Een full property CanShowCode die kan ingesteld worden op true or false, om aan te geven of de code van buitenuit kan gezien worden.
+        /// </summary>
+        public bool CanShowCode
+        {
+            get { return canShowCode; }
+            set { canShowCode = value; }
+        }
+
+        /// <summary>
+        /// Een read-only property CodeLevel van type int. 
+        /// Deze property zal het level van de code teruggeven. 
+        /// Het level is eenvoudigweg de code gedeeld door 1000 als geheel getal (dus indien de code 500 is zal 0 worden teruggegeven, indien de code 2000 is wordt 2 teruggegeven, etc.)
+        /// </summary>
+        public int CodeLevel { get; private set; }
+
+
+        /// <summary>
+        /// Een fullproperty Code met private set. 
+        /// De get van deze property zal -666 teruggeven, tenzij CanShowcode op true staat, in dit geval zal de effectieve code worden terug gegeven.
+        /// </summary>
+        public int Code
+        {
+            get
+            {
+                int result = -666;
+                if (CanShowCode)
+                {
+                    result = code;
+                }
+                return result;
+            }
+            private set
+            {
+                code = value;
+            }
+        }
+
+
+        /// <summary>
+        /// Een methode TryCode die een geheel getal als parameter aanvaardt. 
+        /// De methode geeft een true terug indien de code correct was, anders false. 
+        /// Deze methode kan gebruikt worden om extern een code te testen, indien deze overeenkomt met de bewaarde code dan zal gemeld worden dat de code geldig is en wordt ook getoond hoeveel keer de gebruiker geprobeerd heeft. 
+        /// Indien de gebruiker -666 meegaf dan meldt de methode dat de gebruiker een cheater is . 
+        /// Indien de gebruiker een foute code meegaf dan meldt de methode dat dit een foute code was en wordt het aantal pogingen met 1 verhoogd.
+        /// </summary>
+        /// <param name="getal"></param>
+        public bool TryCode(int getal)
+        {
+            bool result = false;
+            aantalPogingen++;
+            if (getal != -666)
+            {
+                if (getal == code)
+                {
+                    Console.WriteLine("Code is geldig.");
+                    Console.WriteLine($"De gebruiker heeft {aantalPogingen} keer geprobeerd.");
+                    result = true;
+                    aantalPogingen = 0;
+                }
+                else
+                {
+                    Console.WriteLine("Foute code");
+                }
+            }
+            else // getal == -666
+            {
+                Console.WriteLine("Je bent een cheater.");
+            }
+            return result;
+        }
+
+        private int aantalPogingen = 0;
+    }
+}
+
---
+++ ConsoleDigitaleKluis22sep2023/Program.cs
@@ -0,0 +1,75 @@
+﻿using ConsoleDigitaleKluis22sep2023;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welkom bij de digitale kluis!");
+
+        {
+            DigitaleKluis k1 = new DigitaleKluis(500);
+            Console.WriteLine("k1.CodeLevel = " + k1.CodeLevel); // 0
+            Console.WriteLine("k1.Code = " + k1.Code);
+            k1.CanShowCode = true;
+            Console.WriteLine("k1.Code = " + k1.Code);
+        }
+
+
+        Console.WriteLine("\nDruk op de enter toets om verder te gaan.");
+        Console.ReadLine();
+
+        {
+            DigitaleKluis k2 = new DigitaleKluis(2000);
+            Console.WriteLine("k2.CodeLevel = " + k2.CodeLevel); // 2
+            Console.WriteLine("k2.Code = " + k2.Code);
+            k2.CanShowCode = true;
+            Console.WriteLine("k2.Code = " + k2.Code);
+        }
+
+
+        Console.WriteLine("\nDruk op de enter toets om verder te gaan.");
+        Console.ReadLine();
+
+
+        {
+            DigitaleKluis k3 = new DigitaleKluis(2000);
+            Console.WriteLine("k3.CodeLevel = " + k3.CodeLevel); // 2
+            Console.WriteLine("k3.Code = " + k3.Code);
+            Console.WriteLine(k3.TryCode(1234));
+            Console.WriteLine(k3.TryCode(2000));
+            Console.WriteLine(k3.TryCode(5632));
+            Console.WriteLine(k3.TryCode(9985));
+            Console.WriteLine(k3.TryCode(2000));
+            Console.WriteLine(k3.TryCode(-666));
+
+            k3.CanShowCode = true;
+            Console.WriteLine("k3.Code = " + k3.Code);
+        }
+
+        Console.WriteLine("\nDruk op de enter toets om verder te gaan.");
+        Console.ReadLine();
+
+        {
+            DigitaleKluis k4 = new DigitaleKluis(6578);
+            Console.WriteLine(k4.TryCode(6578));
+            Console.WriteLine(k4.TryCode(6578));
+            Console.WriteLine(k4.TryCode(6578));
+            Console.WriteLine(k4.TryCode(9985));
+            Console.WriteLine(k4.TryCode(2000));
+            Console.WriteLine(k4.Code);
+            Console.WriteLine(k4.TryCode(6578));
+        }
+
+        Console.WriteLine("\nDruk op de enter toets om verder te gaan.");
+        Console.ReadLine();
+
+
+        {
+            DigitaleKluis k5 = new DigitaleKluis(123456);
+            k5.CanShowCode = false;
+            Console.WriteLine(k5.TryCode(123456));
+        }
+
+        Console.ReadLine();
+    }
+}
