82713d3598ac07361f278d598e80c959f27d07d4
kris_borre <kris_borre@DESKTOP-V0LQE95>
2023-09-25 15:18:52 +0200

    abstract class and project reference

---
+++ ConsoleDierenFilter25sep2023/Pig.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleDierentuin25sep2023;
+
+namespace ConsoleDierenFilter25sep2023
+{
+    internal class Pig : Mammal
+    {
+        public override void Speaks()
+        {
+            Console.WriteLine("ork ork");
+        }
+
+        public override string ToString()
+        {
+            return "Varken";
+        }
+    }
+}
