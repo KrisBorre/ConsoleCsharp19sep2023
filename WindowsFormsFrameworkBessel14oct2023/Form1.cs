5f0da0110b6ddece36b580d9fa91927d5621a444
kris_borre <kris_borre@DESKTOP-V0LQE95>
2023-10-14 13:59:11 +0200

    Bessel function graph

---
+++ WindowsFormsFrameworkBessel14oct2023/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsFrameworkBessel14oct2023
+{
+    public partial class Form1 : Form
+    {
+        private Chart chart2;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Add Reference -> Assemblies -> Framework -> System.Windows.Forms.DataVisualization -> check
+
+            Text = "Bessel function";
+
+            ChartArea chartArea2 = new ChartArea();
+            Series series2 = new Series();
+            chart2 = new Chart();
+            chartArea2.Name = "ChartArea2";
+            chart2.ChartAreas.Add(chartArea2);
+            chart2.Location = new Point(66, 35);
+            chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea2";
+            series2.ChartType = SeriesChartType.Point;
+            series2.Legend = "legend2";
+            series2.Name = "series2";
+
+            Bessel14oct2023_jy bessjy = new Bessel14oct2023_jy();
+
+            const double minimum = -5;
+            const double maximum = 15;
+            const int AANTAL = 1000;
+            for (int i = 1; i <= AANTAL; i++)
+            {
+                double x = (i / (double)AANTAL) * (maximum - minimum) + minimum;
+
+                series2.Points.Add(new DataPoint(x, bessjy.j0(x)));
+            }
+
+            chart2.Dock = DockStyle.Fill;
+            chart2.Series.Add(series2);
+            chart2.Size = new Size(300, 300);
+            chart2.TabIndex = 0;
+            chart2.Text = "chart2";
+
+            Controls.Add(chart2);
+        }
+    }
+}
---
+++ WindowsFormsFrameworkBessel14oct2023/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsFrameworkBessel14oct2023
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        public static double sqrt(double x)
+        {
+            return Math.Pow(x, 0.5);
+        }
+
+        public static double abs(double x)
+        {
+            return Math.Abs(x);
+        }
+
+        public static double cos(double x)
+        {
+            return Math.Cos(x);
+        }
+
+        public static double sin(double x)
+        {
+            return Math.Sin(x);
+        }
+
+        public static double log(double x)
+        {
+            return Math.Log(x);
+        }
+    }
+}
